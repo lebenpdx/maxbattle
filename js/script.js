b12363f147337efb212e6209dcf99ef0c9ea4a08
lebenpdx <benjaminle97@gmail.com>
2025-04-29 12:53:09 -0700

    PokeAPI request for base stats

--- js/script.js
+++ js/script.js
@@ -2,14 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("ivForm");
 
     form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent form from reloading the page
+        event.preventDefault();
 
         const attack = parseInt(document.getElementById("attackIV").value, 10);
         const defense = parseInt(document.getElementById("defenseIV").value, 10);
         const stamina = parseInt(document.getElementById("staminaIV").value, 10);
+        const pokemonName = document.getElementById("pokemonName").value.trim().toLowerCase();
 
-        console.log("Submitted IVs:", { attack, defense, stamina });
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+            .then(response => {
+                console.log(`fetching ${pokemonName}`);
+                if(!response.ok) {
+                    throw new Error("Pokemon not found");
+                }
+                return response.json();
+            })
+            .then(data => {
+                data.stats.forEach(stat => {
+                    console.log(`${stat.stat.name}: ${stat.base_stat}`)
+                })
+            })
+            .catch(error => {
+                console.error("Error:",error.message);
+            })
 
-        console.log(`IVs submitted:\nAttack: ${attack}\nDefense: ${defense}\nStamina: ${stamina}`);
+        console.log(`${pokemonName} IVs:`, { attack, defense, stamina });
     });
 });
