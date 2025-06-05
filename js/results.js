ea469d8c435bae032fa07e24c90c45bdeba22df4
lebenpdx <benjaminle97@gmail.com>
2025-06-05 09:45:05 -0700

    Display data in form of table

--- js/results.js
+++ js/results.js
@@ -1,18 +1,17 @@
 window.addEventListener("DOMContentLoaded", async function () {
 	const boss = new URLSearchParams(window.location.search).get("boss");
-	const storedPokemonData = this.sessionStorage.getItem("storedPokemonData");
+	const storedPokemonData = sessionStorage.getItem("storedPokemonData");
 	const pokemonData = JSON.parse(storedPokemonData);
 	const galleryContainer = document.getElementById("galleryContainer");
 	const bossData = await pogoAPI2(boss);
 	finalresults = await generateDamageRankings(pokemonData, bossData);
 	for (result of finalresults) {
+		/*
 		const newResult = document.createElement("div");
 		newResult.className = "tempResult";
 		newResult.style.backgroundColor = "#" + Math.floor(Math.random(1) * 16777215).toString(16);
 
-		const folder = result.name.includes("GIGANTAMAX-") ? "gmax" : "dmax";
-		const img = document.createElement("img");
-		img.src = `assets/images/${folder}/${result.name}.webp`;
+		
 		img.style.width = "100%";
 		img.classList.add("galleryImg");
 
@@ -22,15 +21,39 @@ window.addEventListener("DOMContentLoaded", async function () {
 		const bestType = document.createElement("p");
 		bestType.textContent = `Best Attack ${result.type}`;
 
-		const damage = document.createElement("p");
-		damage.textContent = `Damage: ${result.damage}`;
+		//const damage = document.createElement("p");
+		//damage.textContent = `Damage: ${result.damage}`;
 
 		newResult.classList.add("gallery");
 		newResult.append(img);
 		newResult.append(pokemonName);
 		newResult.append(bestType);
-		newResult.append(damage);
+		//newResult.append(damage);
 
 		galleryContainer.append(newResult);
+*/
+		const tableTarget = document.getElementById("resultsTable");
+
+		//const rankingTable = document.createElement("table");
+
+		const row = document.createElement("tr");
+		const name = document.createElement("td");
+		const damage = document.createElement("td");
+		const move = document.createElement("td");
+		const folder = result.name.includes("GIGANTAMAX-") ? "gmax" : "dmax";
+		const img = document.createElement("img");
+		img.src = `assets/images/${folder}/${result.name}.webp`;
+		name.append(img);
+		name.append(result.name);
+		damage.innerText = result.damage;
+		move.innerText = result.type;
+
+		row.append(name);
+		row.append(damage);
+		row.append(move);
+
+		tableTarget.append(row);
+
+		//console.log(result);
 	}
 });
