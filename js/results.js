eb3adeb24460d787001401f37f1b1453f7377cbd
lebenpdx <benjaminle97@gmail.com>
2025-06-09 00:08:32 -0700

    Added Tabs

--- js/results.js
+++ js/results.js
@@ -7,6 +7,9 @@ window.addEventListener("DOMContentLoaded", async function () {
 	const defenseResults = await generateDefenseRankings(pokemonData, bossData);
 
 	finalresults.forEach((result, i) => {
+		if (i >= 50) {
+			return;
+		}
 		const tableTarget = document.getElementById("resultsTable");
 
 		const row = document.createElement("tr");
@@ -34,6 +37,9 @@ window.addEventListener("DOMContentLoaded", async function () {
 	});
 
 	defenseResults.forEach((result, i) => {
+		if (i >= 50) {
+			return;
+		}
 		const tableTarget = document.getElementById("defenseTable");
 
 		const row = document.createElement("tr");
@@ -56,4 +62,28 @@ window.addEventListener("DOMContentLoaded", async function () {
 
 		tableTarget.append(row);
 	});
+
+	document.querySelector(".tablinks").click();
 });
+
+// https://www.w3schools.com/howto/howto_js_tabs.asp
+function openTable(evt, cityName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabContent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
